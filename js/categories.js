426fa6188698b1c8dd9164c7e2c857f22c9c52ec
AsifKhan-27 <asifak001@gmail.com>
2022-09-04 00:18:08 +0600

    added category names section

---
+++ js/categories.js
@@ -0,0 +1,22 @@
+const loadCategories=()=>{
+	const url=`https://openapi.programming-hero.com/api/news/categories`;
+	fetch(url)
+	.then(res=>res.json())
+	.then(data=>displayCategories(data.data.news_category))
+	.catch(error=>console.log(error))
+}
+
+const displayCategories=(categories)=>{
+	// console.log(categories);
+	const categoryNames=document.getElementById('category-names');
+	for(const category of categories){
+		// console.log(category.category_name);
+		const categoryNameDiv=document.createElement('div');
+		categoryNameDiv.innerHTML=`
+			<span>${category.category_name}</span>
+		`;
+		categoryNames.appendChild(categoryNameDiv);
+	}
+}
+
+loadCategories()
